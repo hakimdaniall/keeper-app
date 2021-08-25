374744bd161f1cf6d45856c49883020ab76f1381
Hakim Danial <dev.hakimdanial@gmail.com>
2021-08-25 14:39:20 +0800

    mapping

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
-function Note(){
+function Note(props){
     return (
         <div class="note">
-            <h1>This is the title</h1>
-            <p>This is the content</p>
+            
+            <h1><span>{props.id}</span>{props.title}</h1>
+            <p>{props.content}</p>
         </div>
     );
 }
