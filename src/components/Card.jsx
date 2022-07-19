fe3cee70825934e6c572a5a282bcbd10f898e1e7
Hakim Danial <dev.hakimdanial@gmail.com>
2022-07-19 22:49:41 +0800

    remove classname error

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Card(props) {
     return (
-        <div class="note full-width--">
+        <div className="note full-width--">
             <h1><span>{props.id}</span> {props.category}</h1>
             <p>{props.joke}</p>
         </div>
